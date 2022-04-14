76b9580b5d13383e3528a206e4efb6e69d091424
Luan <luan@plannerti.com.br>
2022-04-14 16:21:31 -0300

    adding cors

--- GameAPI/Startup.cs
+++ GameAPI/Startup.cs
@@ -30,6 +30,15 @@ namespace GameAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
             services.AddControllers();
 
             services.AddSwaggerGen(c =>
@@ -62,6 +71,8 @@ namespace GameAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
 
