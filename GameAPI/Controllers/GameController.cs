8f1ce857e7c7b94d1f888c1bf0e00ce56b7e59c4
LuanSantos00 <luancosmefilho@gmail.com>
2022-04-19 16:02:37 -0300

    adjustings

--- GameAPI/Controllers/GameController.cs
+++ GameAPI/Controllers/GameController.cs
@@ -1,9 +1,7 @@
 ﻿using GameAPI.Models;
 using GameAPI.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GameAPI.Controllers
